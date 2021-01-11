197d7d49ad2497a015eed9a04d3f950ad889f2dd
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-11 16:26:32 +0900

    useEffect loading

--- next/pages/Loading.jsx
+++ next/pages/Loading.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import loadingStyle from '../styles/loading.module.scss'
 
 const Loading = () => {
+
+    useEffect(() => {
+        const loading = document.querySelector("div")
+        loading.animate({
+            opacity: [0, 1]
+        }, 1500)
+    });
+
     return(
-        <div className={loadingStyle.nowl}>Now Loading...</div>
+            <section className={loadingStyle.body}>
+              <div className={loadingStyle.nowl}>Now Loading...</div>
+            </section>
     )
 }
 
