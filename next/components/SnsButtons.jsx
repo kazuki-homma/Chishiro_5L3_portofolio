2b7e6db8fe235fe8b47775c372959d3e7e19d84a
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-17 23:55:29 +0900

    make snsbuttoncomponent

--- next/components/SnsButtons.jsx
+++ next/components/SnsButtons.jsx
@@ -1,1 +1,25 @@
-const Sns
+import { Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { faTwitter } from '@fortawesome/fontawesome-free';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconContext } from "react-icons";
+import { FaTwitter, FaInstagram, FaFacebook} from 'react-icons/fa';
+
+const SnsButtons = (props) => {
+    return (
+        <div className={props.snsButtons}>
+        <Toolbar className={props.snsToolBar}>
+          <IconButton edge="start" className={props.snsMark} color="inherit" aria-label="menu">
+            <FaTwitter />
+          </IconButton>
+          <IconButton edge="start" className={props.snsMark} color="inherit" aria-label="menu">
+            <FaInstagram />
+          </IconButton>
+          <IconButton edge="start" className={props.snsMark} color="inherit" aria-label="menu">
+            <FaFacebook />
+          </IconButton>
+        </Toolbar>
+      </div>
+    );
+}
+
+export default SnsButtons;
