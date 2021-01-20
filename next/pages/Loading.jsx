d9b60e0731d0691740b1c35307551133b1f7f58c
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-20 18:13:58 +0900

    loading animation

--- next/pages/Loading.jsx
+++ next/pages/Loading.jsx
@@ -28,7 +28,19 @@ const Loading = () => {
                     ...transitionStyles[state]
                 }}>
                     <div className={loadingStyle.body}>
-                      <div className={loadingStyle.spinner}>
+                      <div className={loadingStyle.loadingTitle}>
+                        <span className={loadingStyle.char}>C</span>
+                        <span className={loadingStyle.char}>h</span>
+                        <span className={loadingStyle.char}>i</span>
+                        <span className={loadingStyle.char}>s</span>
+                        <span className={loadingStyle.char}>h</span>
+                        <span className={loadingStyle.char}>i</span>
+                        <span className={loadingStyle.char}>r</span>
+                        <span className={loadingStyle.char}>o</span>
+                        <span className={loadingStyle.char}>_</span>
+                        <span className={loadingStyle.char}>5</span>
+                        <span className={loadingStyle.char}>L</span>
+                        <span className={loadingStyle.char}>3</span>
                       </div>
                     </div>
                 </div>
